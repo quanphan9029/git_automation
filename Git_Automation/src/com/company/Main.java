50bbf78cf36e1a64fb4fece665688d0ff4534dd9
quanphan9029 <55967910+quanphan9029@users.noreply.github.com>
2019-11-19 18:10:20 +0700

    Add files via upload

---
+++ Git_Automation/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        WebDriver driver = new ChromeDriver();
+    }
+}
